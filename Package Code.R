fbf72c864119855ad85e9796f288ea2b85c4cdd6
YouKnowJP <101751705+YouKnowJP@users.noreply.github.com>
2023-12-11 16:43:50 -0800

    Update Package Code.R
    Syntax changes

--- Package Code.R
+++ Package Code.R
@@ -22,22 +22,25 @@ powerConsum[,6] <- as.numeric(powerConsum[,6])
 powerConsum[,7] <- as.numeric(powerConsum[,7])
 powerConsum[,8] <- as.numeric(powerConsum[,8])
 
-#Constructing Plot1
+# Constructing Plot1
 with(powerConsum, hist(Global_active_power, xlab = "Global Active Power (kilowatts)",
                        col = "red", main = "Global Active Power"))
-#Save plot1 to a PNG file
+
+# Save plot1 to a PNG file
 dev.copy(device = png, width = 480, height = 480, file = "Plot1.png")
 
 #Constructing Plot2..because x-axis is named "Koean" in my laptop, I reset x-axis
 with(powerConsum, plot(DateTime, Global_active_power, type = "l", xlab = "",
                        ylab = "Global Active Power (kilowatts)", xaxt = "n"))
+
 axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
                       as.numeric(powerConsum$DateTime[1441]),
                       as.numeric(powerConsum$DateTime[2880])),labels = c("Thu","Fri","Sat"))
-#Save plot2 to a PNG file
+
+# Save plot2 to a PNG file
 dev.copy(device = png, width = 480, height = 480, file = "Plot2.png")
 
-#Constructing Plot3
+# Constructing Plot3
 with(powerConsum, plot(DateTime, Sub_metering_1, type = "n", xaxt = "n",
                        xlab = "", ylab = "Energy sub metering"))
 axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
@@ -48,24 +51,28 @@ with(powerConsum, lines(DateTime, Sub_metering_2, col = "red"))
 with(powerConsum, lines(DateTime, Sub_metering_3, col = "blue"))
 legend("topright",lty = 1, col=c("black","red","blue"),
        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-#Save plot3 to a PNG file
+
+# Save plot3 to a PNG file
 dev.copy(device = png, width = 480, height = 480, file = "Plot3.png")
 
-#Constructing Plot4
+# Constructing Plot4
 par(mfrow = c(2,2))
-#Plot at topleft
+
+# Plot at topleft
 with(powerConsum, plot(DateTime, Global_active_power, type = "l", xlab = "",
                        ylab = "Global Active Power", xaxt = "n"))
 axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
                       as.numeric(powerConsum$DateTime[1441]),
                       as.numeric(powerConsum$DateTime[2880])),labels = c("Thu","Fri","Sat"))
-#Plot at topright
+
+# Plot at topright
 with(powerConsum, plot(DateTime, Voltage, type = "l", xlab = "datetime", ylab = "Voltage",
                        xaxt = "n"))
 axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
                       as.numeric(powerConsum$DateTime[1441]),
                       as.numeric(powerConsum$DateTime[2880])),labels = c("Thu","Fri","Sat"))
-#Plot at bottomleft
+
+# Plot at bottomleft
 with(powerConsum, plot(DateTime, Sub_metering_1, type = "n", xaxt = "n",
                        xlab = "", ylab = "Energy sub metering"))
 axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
@@ -76,7 +83,8 @@ with(powerConsum, lines(DateTime, Sub_metering_2, col = "red"))
 with(powerConsum, lines(DateTime, Sub_metering_3, col = "blue"))
 legend("topright",lty = 1, col=c("black","red","blue"),
        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-#Plot at bottomright
+
+# Plot at bottomright
 with(powerConsum, plot(DateTime, Global_reactive_power, type= "l", xlab = "datetime", 
                        ylab = "Global_reactive_power", xaxt = "n"))
 axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
